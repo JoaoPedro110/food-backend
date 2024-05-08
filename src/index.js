83f7d26151d30a0a9ec1d79b24f522131122ffa8
João Pedro Magrini <reccojp65@gmail.com>
2024-05-08 16:43:50 -0300

    commmitera

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import {con} from "./config/database.js";
 
 const app = express();
 
@@ -9,7 +10,16 @@ app.use(cors());
 //rotas
 app.get('/olamundo', function(req, res){
     res.status(200).send("Olá Mundo!")
-})
+});
+
+app.get("/produtos", function(req,res){
+    con.query('select * from produto', function(err, result){
+        if (err)
+            return res.status(500).send("Ocorreu um erro: " + err.message);
+        else
+            return res.status(200).json(result);  
+    })
+});
 
 //levanta o server
 app.listen(3001, function(){
